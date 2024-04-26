c0c3c1b5c67fee09d0dbfb3cbfe3b5cf13464cec
Opilane TTHK <opilane@tthk.ee>
2024-04-26 12:08:05 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,12 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("I hate this roundabouty bullshit with uploading github, looney tunes type a grade");
             Console.WriteLine("SKIBIDIDOPDOPYESYES");
+            int y = Convert.ToInt32(Console.Readline());
+            Console.WriteLine("Press 1")
+            if (y = 1){
+                Console.WriteLine("Wow you followed instructions from a computer, you automaton");
+            }
+
         }
     }
 }
